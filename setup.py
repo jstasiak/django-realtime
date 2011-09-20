7ef2d0974115ef15b50c66b1b18f14be423c8aef
Jakub Stasiak <kuba.stasiak@gmail.com>
2011-09-20 22:33:45 +0200

    README file changed from rst to md

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     author = "Jakub Stasiak",
     author_email = "kuba.stasiak@gmail.com",
     description = ("Django application that simplifies usage of socket.io in Django projects"),
-    long_description = open('README.rest').read(),
+    long_description = open('README.md').read(),
     url = "http://github.com/jstasiak/django-realtime",
     py_modules=["realtime",],
     install_requires=["gevent-socketio", "gevent-websocket"],
